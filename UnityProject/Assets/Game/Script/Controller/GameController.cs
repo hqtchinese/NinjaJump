44c8e502e984887076257762745852c92acd72ec
hqtchinese <hqt86834387@163.com>
2019-11-26 22:37:19 +0800

    no message

--- UnityProject/Assets/Game/Script/Controller/GameController.cs
+++ UnityProject/Assets/Game/Script/Controller/GameController.cs
@@ -1,12 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameBase;
 
 namespace NinjaJump
 {
-    public class GameController : MonoBehaviour
+    
+    public class GameController : MonoSingleton<GameController>
     {
+        public Vector2 ViewPos;
+        public GameState State { get; private set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+        }
+
+        //加载游戏场景中的物体、UI等
+        private void InitGameScene()
+        {
+
+        }
+
         
+
+
     }
 
+    public enum GameState
+    {
+        Ready,
+        Gaming,
+        Pause,
+        GameOver,
+
+    }
 }
---
+++ UnityProject/Assets/Game/Script/Helper/PositionFixer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameBase.Editor;
+
+namespace NinjaJump
+{
+    public class PositionFixer : MonoBehaviour
+    {
+
+        [Range(0,1)]
+        public float Factor;
+
+        public Vector2 Position;
+        private Vector2 ViewPos => GameController.Instance.ViewPos;
+
+        public void Update()
+        {
+            transform.position = (Position - ViewPos) * Factor;
+        }
+
+    }
+
+}
