3a1a9fe621ff7666fa931160ae191d07ae150030
hqtchinese <hqt86834387@163.com>
2019-12-01 21:05:39 +0800

    no message

---
+++ UnityProject/Assets/Base/Scripts/Extensions/VectorExt.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorExt
+{
+    public static Vector2 ToVec2(this Vector3 v3)
+    {
+        return v3;
+    }
+}
--- UnityProject/Assets/Game/Script/Module/Role/RoleActionModule.cs
+++ UnityProject/Assets/Game/Script/Module/Role/RoleActionModule.cs
@@ -7,8 +7,9 @@ namespace NinjaJump
     public class RoleActionModule : Module<RoleDock>
     {
         private RoleAnimeModule m_animeModule;
-        private float m_speed = 3;
+        private float m_speed = 10;
         private Vector2 m_moveDir;
+        private Transform transform;
 
         public RoleActionModule(RoleDock dock) : base(dock)
         {
@@ -18,13 +19,14 @@ namespace NinjaJump
         public override void Awake()
         {
             m_animeModule = m_dock.GetModule<RoleAnimeModule>();
+            transform = m_dock.transform;
         }
 
         public override void Update()
         {
             if (m_dock.Status == RoleStatus.Jump)
             {
-                
+                transform.Translate(m_moveDir * m_speed * Time.deltaTime);
             }
         }
 
@@ -34,8 +36,9 @@ namespace NinjaJump
             if (m_dock.Status != RoleStatus.Aim)
                 return;
 
-            // m_moveDir = targetPos - m_dock.transform.position;
-
+            m_moveDir = targetPos - transform.position.ToVec2();
+            
+            m_moveDir.Normalize();
             m_dock.Status = RoleStatus.Jump;
             m_animeModule.DoAnime();
         }
